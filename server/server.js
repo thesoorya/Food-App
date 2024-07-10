f07f0648e5af26d407b4780cb907ef9574161b6a
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-07-10 16:17:53 +0530

    server modified

--- server/server.js
+++ server/server.js
@@ -4,12 +4,15 @@ const dotenv = require('dotenv').config()
 const cors = require("cors")
 const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db')
+const foodRouter = require('./routes/foodRoute')
 
 const PORT = process.env.PORT || 5000
 
 // mongodb connection
 connectDB()
 
+app.use(express.json())
+app.use('/api', foodRouter)
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
